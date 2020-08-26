7fc3e589bec3587ffb4e2c1de34a550a60752c6e
haran2001 <hari.ayapps@gmail.com>
2020-08-26 18:21:26 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const squares = document.querySelectorAll('.grid div')
+	const width = 15
+	let currentShooterIndex = 202
+
+	
+
+	//add invaders
+
+	const alienInvaders = [0, 2, 4]
+	alienInvaders.forEach(invader => squares[invader].classList.add('invader'))
+
+
+
+	//move shooter based on key codes
+	function moveShooter(e){
+		squares[currentShooterIndex].classList.remove('player')
+		switch(e.keyCode) {
+			case 37: 
+			if(currentShooterIndex % width !== 0)
+				currentShooterIndex -= 1
+			break
+
+			case 39:
+			if(currentShooterIndex % width < width - 1)
+				currentShooterIndex += 1
+			break
+		}
+		squares[currentShooterIndex].classList.add('player')
+	}
+
+	document.addEventListener('keydown', moveShooter)
+
+})
