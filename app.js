bba730269b86fd5aab39508a9dd7f433746e5a99
haran2001 <hari.ayapps@gmail.com>
2020-08-27 18:50:25 +0530

    decend function

--- app.js
+++ app.js
@@ -2,17 +2,26 @@ document.addEventListener("DOMContentLoaded", () => {
 	const squares = document.querySelectorAll('.grid div')
 	const width = 15
 	let currentShooterIndex = 202
-
+	var myInterval
+	var invaderDirection = 0
 	
 
 	//add invaders
-	const alienInvaders = [0]
+	const alienInvaders = [15 , 16]
 	alienInvaders.forEach(invader => squares[invader].classList.add('invader'))
 
+	//decend invader
+	function decend(){
+		if(invaderDirection < 210){
+			alienInvaders.forEach(index => squares[index + invaderDirection-width].classList.remove('invader'))
+			alienInvaders.forEach(index => squares[index + invaderDirection].classList.add('invader'))
+			invaderDirection += width
+		}else{
+			clearInterval(myInterval)
+			console.log('cleared')
+		}
 
-	//decend invaders
-	alienInvaders.forEach(index => squares)
-
+	}
 	//move shooter based on key codes
 	function moveShooter(e){
 		squares[currentShooterIndex].classList.remove('player')
@@ -30,6 +39,9 @@ document.addEventListener("DOMContentLoaded", () => {
 		squares[currentShooterIndex].classList.add('player')
 	}
 
+	myInterval = setInterval(decend, 1000)
+	// myInterval
 	document.addEventListener('keydown', moveShooter)
+
 
 })
