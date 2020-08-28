29cb54112f0cb0a0cedab1ac381055020843e1b2
haran2001 <hari.ayapps@gmail.com>
2020-08-28 18:14:40 +0530

    new functions

--- app.js
+++ app.js
@@ -2,26 +2,28 @@ document.addEventListener("DOMContentLoaded", () => {
 	const squares = document.querySelectorAll('.grid div')
 	const width = 15
 	let currentShooterIndex = 202
-	var myInterval
+	var alienInterval
+	var bulletInterval
 	var invaderDirection = 0
-	
-
+	let currentBulletIndex
+	let alienInvaders = [15, 16]
 	//add invaders
-	const alienInvaders = [15 , 16]
+
 	alienInvaders.forEach(invader => squares[invader].classList.add('invader'))
 
 	//decend invader
 	function decend(){
-		if(invaderDirection < 210){
-			alienInvaders.forEach(index => squares[index + invaderDirection-width].classList.remove('invader'))
-			alienInvaders.forEach(index => squares[index + invaderDirection].classList.add('invader'))
-			invaderDirection += width
-		}else{
-			clearInterval(myInterval)
-			console.log('cleared')
+		if(alienInvaders[alienInvaders.length - 1] > 210){
+			alienInvaders.forEach(index => squares[index].classList.remove('invader'))
+			alienInvaders.forEach(index => index += 15)
+			alienInvaders.forEach(index => squares[index].classList.add('invader'))
 		}
 
+		else{
+			resetInvaders()
+		}
 	}
+	
 	//move shooter based on key codes
 	function moveShooter(e){
 		squares[currentShooterIndex].classList.remove('player')
@@ -39,9 +41,44 @@ document.addEventListener("DOMContentLoaded", () => {
 		squares[currentShooterIndex].classList.add('player')
 	}
 
-	myInterval = setInterval(decend, 1000)
-	// myInterval
+	function shoot(){
+		if(currentBulletIndex > width){
+			console.log(currentBulletIndex)
+			squares[currentBulletIndex].classList.remove('boom')
+			currentBulletIndex -= width
+			squares[currentBulletIndex].classList.add('boom')
+			squares[currentBulletIndex + 15].classList.remove('boom')
+			collisionCheck()
+		}
+		else{
+			squares[currentBulletIndex].classList.remove('boom')
+			clearInterval(bulletInterval)
+		}
+	}
+
+	function shootCall(){
+		console.log(currentShooterIndex)
+		currentBulletIndex = currentShooterIndex
+		bulletInterval = setInterval(shoot, 500)
+	}
+
+	function collisionCheck(){
+		for(let i=0; i<alienInvaders.length; i++){
+			if(alienInvaders[i] === currentBulletIndex){
+				squares[i].classList.remove('invader')
+				squares[i].classList.add('boom')
+       			setTimeout(() => squares[currentBulletIndex].classList.remove('boom'), 250)
+			}
+		}
+
+	}
+
+	function resetInvaders(){
+		alienInvaders = [15 , 16]
+	}
+	
+	alienInterval = setInterval(decend, 1000)
 	document.addEventListener('keydown', moveShooter)
-
-
+	document.addEventListener('click', shootCall)
+	console.log("Hello world")
 })
